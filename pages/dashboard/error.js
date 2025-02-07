a20fc37a0f7fe576dcb455dab6578f4632f3f763
Arinzechukwu <arize1524@gmail.com>
2025-02-07 15:03:54 +0100

    7 feb 3:03

--- pages/dashboard/error.js
+++ pages/dashboard/error.js
@@ -1,12 +1,12 @@
 import {Button,Link} from "@mui/material"
 import {Cancel,ArrowBack} from "@mui/icons-material"
 
- const Error=()=>{
+ const Error = ()=>{
     return(<>
     <div style={{height:"100lvh",width:"100vw"}} className="flex  flex-row items-center justify-center">
     <div className="flex flex-col gap-8 justify-center items-center">
     <Cancel sx={{color:"red",height:"130px",width:"130px"}}/>
-      <div className="">Something went wrong {" "+" "+details.error}</div>
+      <div className="">An error occured</div>
       <Link href={"/dashboard"} className="rubik-b mt-8">{<Button startIcon={<ArrowBack/> } variant="contained" sx={{textTransform:"none",backgroundColor:"#1E3A5F"}}>Home</Button>}</Link>
         </div>
 </div>
--- pages/sucess.js
+++ pages/sucess.js
@@ -1,9 +1,9 @@
 import { Cancel, CheckCircle } from "@mui/icons-material"
 import Head from "next/head";
 
-const Sucess = ()=>{
+const Success = ()=>{
     return(<>
-    <Head><title>Sucess</title></Head>
+    <Head><title>Success</title></Head>
 <div style={{height:"100lvh",width:"100vw"}} className="flex  flex-row items-center justify-center">
     <div className="flex flex-col gap-8 justify-center items-center">
     <Cancel sx={{color:"red",height:"130px",width:"130px"}}/>
