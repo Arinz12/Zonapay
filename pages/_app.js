906d28d57c58a26a0f1ecb2b470d10ee17ad60b9
Arinzechukwu <arize1524@gmail.com>
2025-03-24 10:52:08 +0100

    installable3

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,18 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import '../styles/globals.css'
 import { useRouter } from 'next/router';
 import Footer from '../components/Footer';
-import Script from "/next/script"
+
 
 function MyApp({ Component, pageProps }) {
   const router=useRouter()
+  useEffect(()=>{
+    if ('serviceWorker' in navigator) {
+      navigator.serviceWorker.register('/worker.js')
+        .then(registration => console.log('Service Worker registered'))
+        .catch(error => console.log('Registration failed:', error));
+  }
+  })
 const pages=["/dashboard","/dashboard/settings","/dashboard/history"]
   return(<>
   <Script src="js/pwa.js" strategy="beforeInteractive" />
