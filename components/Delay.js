2653803aaba9e7a4f3c5f7231ca59af7854bc0d2
Arinzechukwu <arize1524@gmail.com>
2025-03-31 18:38:49 +0100

    31 march 2025 8

--- components/Delay.js
+++ components/Delay.js
@@ -1,5 +1,5 @@
 const Delay=()=>{
-    return(<div  style={{backgroundColor:"rgba(0, 0, 0, 0.253)",backdropFilter:"blur(9px)"}} className=" flex-col items-center justify-center  fixed  z-10 w-full bottom-0 h-full shp">
+    return(<div  style={{backgroundColor:"rgba(0, 0, 0, 0.253)",backdropFilter:"blur(9px)"}} className=" flex-col items-center justify-center  fixed  z-10 w-full bottom-0 left-0 right-0 h-full shp">
 <section className="dots-container">
   <div className="dot"></div>
   <div className="dot"></div>
--- pages/dashboard/index.js
+++ pages/dashboard/index.js
@@ -40,7 +40,7 @@ const Dashboard = ({obj}) => {
 <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true"/>
 <link href="https://fonts.googleapis.com/css2?family=Rubik:ital,wght@0,300..900;1,300..900&display=swap" rel="stylesheet"/>
     </Head>
-    <div style={{backgroundColor:"whitesmoke",height:"100dvh"}} >
+    <div style={{backgroundColor:"whitesmoke",height:"100vh"}} >
      <div
          id="createpin" className='hidden  h-full justify-center z-10 items-center fixed top-0 w-full '><Paper elevated={12}   className="relative w-2/3  mx-auto p-7  flex flex-col justify-center items-center gap-2" ><div className="rubik-b">Pin is required </div>
         <Button onClick={()=>{router.push("/dashboard/settings/pin")}} variant={"contained"}>Create pin</Button>
