b161539708c7ba25163f006d2ea2eefce5d30335
Arinzechukwu <arize1524@gmail.com>
2025-03-31 13:04:59 +0100

    31 march 2025 2

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import '../styles/globals.css'
 import { useRouter } from 'next/router';
 import Footer from '../components/Footer';
