cd59ed6dcd8e23cc9dfe53dc093b04c7e65cc05d
Arinzechukwu <arize1524@gmail.com>
2025-04-18 23:24:19 +0100

    11:24pm

--- pages/api/verify.js
+++ pages/api/verify.js
@@ -5,18 +5,24 @@ import {URL} from "url"
 
 export default async function handler(req,res){
 if (req.method==="POST"){
+    console.log(req.body)
     const {iuc,provider,vid}=req.body
 const url=`https://api.flutterwave.com/v3/bill-items/${vid}/validate?code=${provider}&customer=${iuc}`
 
 try{
-const resp= await fetch(url.toString(),{method:"GET",
+const resp= await fetch(url,{method:"GET",
 headers:{
     "Content-Type":"application/json",
     "Authorization":`Bearer ${process.env.FLW_SECRET_KEY}`
 }
 })
+if(resp.ok){
 const resp1= await resp.json()
-res.status(200).json(resp1);}
+res.status(200).json(resp1);
+}
+else{
+    res.status(400).end()
+}}
 catch(e){
 res.status(404);
 }
