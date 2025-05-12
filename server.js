061f451a028ef9785ceef230135877e866fb4508
Arinzechukwu <arize1524@gmail.com>
2025-05-12 16:10:36 +0100

    updated mongodb and mongoose3

--- server.js
+++ server.js
@@ -34,7 +34,7 @@ const { updateFlid,Flid } = require("./Svr_fns/FlutterwaveIds");
 mongoose.set("strictQuery",false)
 //DB CONNECTION
 
-mongoose.connect(process.env.DATABASEURL, {tls:true,connectTimeoutMS: 50000,serverSelectionTimeoutMS:15000,socketTimeoutMS:45000 })
+mongoose.connect(process.env.DATABASEURL , { useNewUrlParser: true, useUnifiedTopology: true,connectTimeoutMS: 50000,serverSelectionTimeoutMS:5000,socketTimeoutMS:45000 })
 .then(() => console.log('MongoDB connected successfully'))
 .catch(err => console.error('MongoDB connection error:', err));
 
