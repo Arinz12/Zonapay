a2ad0fbff994b38545f86b886d5d7447a37460de
Arinzechukwu <arize1524@gmail.com>
2025-05-14 14:08:53 +0100

    track1

--- Svr_fns/Dailyearn.js
+++ Svr_fns/Dailyearn.js
@@ -4,15 +4,15 @@ mongoose.set("strictQuery",false)
 const Eschema=new mongoose.Schema({
     Date:{type:String,unique:true},
     Earning:{type:Number},
-    Airtime_purchases:{type:Number},
-    Data_purchases:{type:Number},
-Cabletv_purchases:{type:Number},
-Electricity_purchases:{type:Number},
-Airtime:{type:Number},
-Data:{type:Number},
-Cable_tv:{type:Number},
-Electricity:{type:Number},
-Total:{type:Number}
+    Airtime_purchases:{type:Number,default:0},
+    Data_purchases:{type:Number,default:0},
+Cabletv_purchases:{type:Number,default:0},
+Electricity_purchases:{type:Number,default:0},
+Airtime:{type:Number,default:0},
+Data:{type:Number,default:0},
+Cable_tv:{type:Number,default:0},
+Electricity:{type:Number,default:0},
+Total:{type:Number,default:0}
 
 })
 
