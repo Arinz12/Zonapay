36bf9f967f41e5d5c4a91be7e056273d3a5bb031
Arinzechukwu <arize1524@gmail.com>
2025-06-06 04:19:47 +0100

    4:19pm

--- Svr_fns/logout.js
+++ Svr_fns/logout.js
@@ -1,8 +1,8 @@
 const mongoose=require("mongoose");
 const { User } = require("./createuser");
 mongoose.set("strictQuery",false);
-const sessions=mongoose.connection.db.collection("sessionsforusers");
 const logout=async (email)=>{
+const sessions= mongoose.connection.db.collection("sessionsforusers");
 const device=await User.findOne({Email:email})
 const id=device._id.toString()
  await sessions.deleteMany({
