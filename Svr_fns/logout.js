424816e3d5a900c178374d68cc19e9934487d652
Arinzechukwu <arize1524@gmail.com>
2025-07-05 09:24:10 +0100

    9:24pm

--- Svr_fns/logout.js
+++ Svr_fns/logout.js
@@ -1,10 +1,10 @@
 const mongoose=require("mongoose");
 const { User } = require("./createuser");
 mongoose.set("strictQuery",false);
-async function logout (id){
+async function logout (iddd){
   console.log("about to search for device session")
 const sessions= mongoose.connection.db.collection("sessionsforusers");
-const device=await User.findOne({Loginid:id})
+const device=await User.findOne({Loginid:iddd})
 const id=device._id.toString()
 console.log("user is ", id)
  await sessions.deleteMany({
